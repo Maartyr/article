654a336587610f0c986e323744a8a13497ba7f33
Igor Silva <igorsillva@hotmail.com.br>
2022-03-09 15:59:12 -0300

    Second model

--- db/migrate/20220309181815_create_comments.rb
+++ db/migrate/20220309181815_create_comments.rb
@@ -1,7 +1,7 @@
-class CreateArticles < ActiveRecord::Migration[6.1]
+class CreateComments < ActiveRecord::Migration[6.1]
   def change
-    create_table :articles do |t|
-      t.string :title
+    create_table :comments do |t|
+      t.string :commenter
       t.text :body
       t.references :article, null: false, foreign_key: true
 
